35053e03f48ca4008d9a72bd0cd8464ce4ca8acf
Rohit <rohitnarnolia88@gmail.com>
2026-01-22 14:01:29 +0530

    refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,24 @@
 #![allow(unused, dead_code)]
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut ans: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            ans.push(line);
-        }
-    }
-    ans
+    // let mut ans: Vec<&str> = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         ans.push(line);
+    //     }
+    // }
+    // ans
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut ans: Vec<&str> = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query.to_lowercase()) {
-            ans.push(line);
-        }
-    }
-    ans
+    let query = query.to_lowercase();
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,21 @@ struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not eoungh arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         Ok(Config {
             query,
             file_path,
@@ -27,7 +34,8 @@ impl Config {
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem passing arguments: {err}");
         process::exit(1);
     });
